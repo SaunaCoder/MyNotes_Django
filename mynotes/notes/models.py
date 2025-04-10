3d9d8b811a6dce0d07dbc4fe988d5fa970104331
Oleksandr Krutko <oleksandr.krutko07@gmail.com>
2025-04-10 20:15:04 +0300

    Create Note model

--- mynotes/notes/models.py
+++ mynotes/notes/models.py
@@ -1,3 +1,12 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
+
+class Note(models.Model):
+    title = models.CharField(max_length=50)
+    text = models.TextField()
+    author = models.ForeignKey(User, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.title
