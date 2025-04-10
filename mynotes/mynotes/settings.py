bdbd667dee5fbda1d105c55c5bfd8ca8f1c6aa8d
Oleksandr Krutko <oleksandr.krutko07@gmail.com>
2025-04-10 19:05:00 +0300

    Create registration system

--- mynotes/mynotes/settings.py
+++ mynotes/mynotes/settings.py
@@ -37,6 +37,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'accounts',
+    'notes', # Remove before accounts commit
 ]
 
 MIDDLEWARE = [
@@ -120,3 +122,7 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+LOGIN_URL = 'login'
+LOGIN_REDIRECT_URL = 'home'
+LOGOUT_REDIRECT_URL = 'login'
